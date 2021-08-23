298da2309e49cbc4bdd8b1c077af12f6a1b35e59
Florian ALU <florian.alu@Digital-Grenoble.local>
2021-08-23 11:52:07 +0200

    feat: Get all characters and get one character by id

--- src/main/java/app/characters/CharacterController.java
+++ src/main/java/app/characters/CharacterController.java
@@ -1,19 +1,35 @@
 package app.characters;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class CharacterController {
-    @GetMapping("/characters")
-    public List<Character> index() {
-        return new ArrayList<>(List.of(
+    private List<Character> characters;
+
+    CharacterController() {
+        this.characters = List.of(
                 new Character(1, "Girouette", CharacterType.Mage, 11),
                 new Character(2, "Pizza", CharacterType.Warrior, 25),
                 new Character(3, "Plouk", CharacterType.Warrior, 19)
-        ));
+        );
+    }
+
+    @GetMapping("/characters")
+    public List<Character> index() {
+        return characters;
+    }
+
+    @GetMapping("/characters/{id}")
+    public Character getCharacterById(@PathVariable(value = "id") int id) {
+        for (Character character : characters) {
+            if (character.getId() == id) {
+                return character;
+            }
+        }
+        return null;
     }
 }
