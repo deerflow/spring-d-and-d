f86dd6c03b8a04181b308513d9590ee00d6bcc6a
Florian <alu.florian@gmail.com>
2021-08-25 14:57:30 +0200

    refacto: Use dao

--- src/main/java/app/characters/CharacterController.java
+++ src/main/java/app/characters/CharacterController.java
@@ -1,5 +1,6 @@
 package app.characters;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -8,41 +9,27 @@ import java.util.List;
 public class CharacterController {
 
     @GetMapping("/characters")
-    public List<Character> getCharacters() {
-        return Characters.list;
+    public List<Character> getCharacters(Model model) {
+        return Characters.findAll();
     }
 
     @GetMapping("/characters/{id}")
     public Character getCharacterById(@PathVariable(value = "id") int id) {
-        for (Character character : Characters.list) {
-            if (character.getId() == id) {
-                return character;
-            }
-        }
-        return null;
+        return Characters.findById(id);
     }
 
     @PostMapping("/characters")
     public int createCharacter(@RequestBody Character character) {
-        Characters.list.add(character);
-        return character.getId();
+        return Characters.createOne(character);
     }
 
-    @PutMapping("/characters")
-    public Character updateCharacter(@RequestBody Character newCharacter) {
-        {
-            for (Character character : Characters.list) {
-                if (character.getId() == newCharacter.getId()) {
-                    Characters.list.set(Characters.list.indexOf(character), newCharacter);
-                    return newCharacter;
-                }
-            }
-            return null;
-        }
+    @PutMapping("/characters/{id}")
+    public boolean updateCharacter(@RequestBody Character character, @PathVariable(value = "id") int id) {
+        return Characters.editOne(id, character);
     }
 
     @DeleteMapping("/characters/{id}")
     public boolean deleteCharacter(@PathVariable(name = "id") int id) {
-        return Characters.list.removeIf(character -> character.getId() == id);
+        return Characters.deleteOne(id);
     }
 }
---
+++ src/main/java/app/characters/CharacterType.java
@@ -0,0 +1,6 @@
+package app.characters;
+
+public enum CharacterType {
+    Warrior,
+    Mage,
+}
